860daea9267cb20fb7a85f4775c56d0c08f55870
Leonardo Scapolan <79846644+LeonardoScapolanMI@users.noreply.github.com>
2021-12-03 18:21:23 +0100

    Added more otions for the lifebar

--- DontStop/Assets/Scripts/RhythmUI/LifeBar.cs
+++ DontStop/Assets/Scripts/RhythmUI/LifeBar.cs
@@ -14,6 +14,15 @@ public class LifeBar : MonoBehaviour
     public float perfectHitBonus;
     private float perfectHitDistanceGained;
 
+    public float barelyHitBonus;
+    private float barelyHitDistanceGained;
+
+    public float worstMissPenality;
+    private float worstMissDistanceLoss;
+
+    public float betterMissPenality;
+    private float betterMissDistanceLoss;
+
     private RectTransform rectTransform;
     private float startingWidth;
     private float widthLimit;
@@ -34,6 +43,9 @@ public class LifeBar : MonoBehaviour
     {
         depletionSpeed = depletionRate * rectTransform.sizeDelta.x;
         perfectHitDistanceGained = perfectHitBonus * rectTransform.sizeDelta.x;
+        barelyHitDistanceGained = barelyHitBonus * rectTransform.sizeDelta.x;
+        worstMissDistanceLoss = worstMissPenality * rectTransform.sizeDelta.x;
+        betterMissDistanceLoss = betterMissPenality * rectTransform.sizeDelta.x;
 
         startingWidth = rectTransform.sizeDelta.x;
         widthLimit = 0;
@@ -70,6 +82,38 @@ public class LifeBar : MonoBehaviour
             newWidth = startingWidth;
         }
         rectTransform.sizeDelta = new Vector2(newWidth, rectTransform.sizeDelta.y);
+    }
+
+    public void BarelyHit()
+    {
+        float newWidth = rectTransform.sizeDelta.x + barelyHitDistanceGained;
+        if (newWidth > startingWidth)
+        {
+            newWidth = startingWidth;
+        }
+        rectTransform.sizeDelta = new Vector2(newWidth, rectTransform.sizeDelta.y);
+    }
+
+    public void WorstMiss()
+    {
+        float newWidth = rectTransform.sizeDelta.x - worstMissDistanceLoss;
+        if (newWidth < widthLimit)
+        {
+            newWidth = widthLimit;
+        }
+        rectTransform.sizeDelta = new Vector2(newWidth, rectTransform.sizeDelta.y);
+        if (newWidth == widthLimit) OnLimitReached();
+    }
+
+    public void BetterMiss()
+    {
+        float newWidth = rectTransform.sizeDelta.x - betterMissDistanceLoss;
+        if (newWidth < widthLimit)
+        {
+            newWidth = widthLimit;
+        }
+        rectTransform.sizeDelta = new Vector2(newWidth, rectTransform.sizeDelta.y);
+        if (newWidth == widthLimit) OnLimitReached();
     }
 
     public void RegisterLimitReachedBehaviour(Action a)
