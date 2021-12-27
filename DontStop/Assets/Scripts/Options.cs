deae1230774e0d162ae492ebba8efc2d17a63feb
Davide Bagnato <davidebagnato97@gmail.com>
2021-12-27 16:31:26 +0100

    Revert "Added first stage"
    This reverts commit 6f5e007fc71c5a584ca46e9f14f073d4d0f64137.

--- DontStop/Assets/Scripts/Options.cs
+++ DontStop/Assets/Scripts/Options.cs
@@ -17,10 +17,7 @@ public class Options : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        if (!tutorial)
-        {
-            TutorialController.instance.hitAlwaysTrue = false;
-        }
+        
     }
 
     // Update is called once per frame
