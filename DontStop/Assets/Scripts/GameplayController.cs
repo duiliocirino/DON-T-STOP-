73dd3dcb453317a6be6b16d16fcf6c8c3e82f94a
duiliocirino <10621933@polimi.it>
2021-12-29 19:20:10 +0100

    sciF

--- DontStop/Assets/Scripts/GameplayController.cs
+++ DontStop/Assets/Scripts/GameplayController.cs
@@ -205,6 +205,7 @@ public class GameplayController : MonoBehaviour
                 TutorialController.instance.disableDialogBox(10);
             }
             TutorialController.instance.hitAlwaysTrue = false;
+            SetCreatorControlActive(false);
 
             screenBlurr.gameObject.SetActive(true);
             // PLANE DISRUPTION
@@ -230,9 +231,11 @@ public class GameplayController : MonoBehaviour
             TutorialController.instance.disableDialogBox(13);
             screenBlurr.gameObject.SetActive(false);
             
+            SetCreatorControlActive(false);
             TutorialController.instance.enableDialogBox(14);
             yield return new WaitForSecondsRealtime(1.5f);
             PlaneHandler.instance.PopPlatform();
+            SetCreatorControlActive(true);
             platformCreated = false;
             while (!platformCreated)
             { 
