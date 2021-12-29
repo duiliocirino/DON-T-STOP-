4b547ac77350c0c2682c215d77df18a8ee29ce8b
Leonardo Scapolan <79846644+LeonardoScapolanMI@users.noreply.github.com>
2021-12-29 18:58:54 +0100

    Added end tutorial point

---
+++ DontStop/Assets/Scripts/EndTutorial.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTutorial : MonoBehaviour
+{
+    private BoxCollider thisCollider;
+
+    private void Awake()
+    {
+        thisCollider = GetComponent<BoxCollider>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            thisCollider.enabled = false;
+            Debug.Log("Rrrrrrrrrrrrrrr");
+        }
+    }
+}
