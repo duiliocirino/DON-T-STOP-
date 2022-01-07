f946437a638a6a8a11d641deca6c5b31ec6effa6
Leonardo Scapolan <79846644+LeonardoScapolanMI@users.noreply.github.com>
2022-01-07 19:04:34 +0100

    Now bad platforms can't spawn outside of bounds

--- DontStop/Assets/Scripts/PlaneHandler.cs
+++ DontStop/Assets/Scripts/PlaneHandler.cs
@@ -55,7 +55,14 @@ public class PlaneHandler : MonoBehaviour
     {
         if (Random.value < probabilityBadPlat)
         {
-            int place = Random.Range(-numberOfEmptyTilesSide, numberOfEmptyTilesSide);
+            print("spawning bad platform");
+            int lowRange = -numberOfEmptyTilesSide;
+            while (position.x + (lowRange * spacing) < -laneNumbersRadius * spacing) lowRange++;
+            int highRange = numberOfEmptyTilesSide;
+            while (position.x + (highRange * spacing) > laneNumbersRadius * spacing) highRange--;
+            print(lowRange + " " + highRange);
+            int place = Random.Range(lowRange, highRange);
+
             Vector3 badPlatformPosition = new Vector3(position.x + (place * spacing), 0.0f, position.z + 2 * spacing);
             if (/*(platformInTutorial * spacing) < badPlatformPosition.z &&*/ badPlatformPosition.z < ((platformInTutorial + platformSkippedAtTutorialEnd + 3) * spacing))
                 return;
