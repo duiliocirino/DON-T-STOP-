f5847f6fcee2079312996a3f31ca07b028fc404d
duiliocirino <10621933@polimi.it>
2022-07-14 17:23:11 +0200

    Restored texts at the beginning of the stages

--- DontStopSubmission/Source/project-directory/DontStop/Assets/Scripts/GameplayController.cs
+++ DontStopSubmission/Source/project-directory/DontStop/Assets/Scripts/GameplayController.cs
@@ -331,9 +331,9 @@ public class GameplayController : MonoBehaviour
         }
         else
         {
-            if (SceneManager.GetActiveScene().name == "Stage1Scene")
+            if (SelectedStage.istance.story && SelectedStage.istance.stageNumber == 1)
             {
-                /*screenBlurr.gameObject.SetActive(true);
+                screenBlurr.gameObject.SetActive(true);
                 TutorialController.instance.enableDialogBox(0);
                 yield return new WaitUntil((() => Input.anyKeyDown && !Pause.paused));
                 TutorialController.instance.disableDialogBox(0);
@@ -341,11 +341,12 @@ public class GameplayController : MonoBehaviour
                 TutorialController.instance.enableDialogBox(1);
                 yield return new WaitUntil((() => Input.anyKeyDown && !Pause.paused));
                 TutorialController.instance.disableDialogBox(1);
-                screenBlurr.gameObject.SetActive(false);*/
+                screenBlurr.gameObject.SetActive(false);
 
                 StartCoroutine(ShowAudienceTutorial());
             }
-            if (SceneManager.GetActiveScene().name == "Stage2Scene")
+            
+            if (SelectedStage.istance.story && SelectedStage.istance.stageNumber == 2)
             {
                 screenBlurr.gameObject.SetActive(true);
                 TutorialController.instance.enableDialogBox(0);
